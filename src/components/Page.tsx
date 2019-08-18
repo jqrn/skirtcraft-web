7e2c1b19f325bba1e50b114e0b13957e0d16dd91
Joe Quarion <2335027+jqrn@users.noreply.github.com>
2019-08-17 21:05:23 -0500

    Twitter card meta tags.

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -33,11 +33,12 @@ export class Page extends React.Component<Props> {
                     <meta property='og:image' content={this.props.openGraphImageUrl || `${BASE_URL}/images/pair.png'`}/>
                     {this.props.openGraphImageUrl == undefined && <meta property='og:image:width' content='1200'/>}
                     {this.props.openGraphImageUrl == undefined && <meta property='og:image:height' content='620'/>}
+                    <meta name='twitter:card' content='summary' />
+                    <meta name='twitter:site' content='@skirtcraft' />
+                    <meta name='twitter:creator' content='@skirtcraft' />
 
                     <link href='https://fonts.googleapis.com/css?family=Muli:400,400i,700,700i&display=swap' rel='stylesheet' type='text/css'/>
                     <link rel='icon' href={ImgFavicon}/>
-
-                    <body />
                 </Helmet>
                 <Header />
                 <NavBar currentTab={this.props.currentTab}/>
