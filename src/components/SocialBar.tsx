15e4b257b4791eda4d34056c6a92f0831779c1fd
Joe Quarion <2335027+jqrn@users.noreply.github.com>
2023-06-22 09:11:24 -0500

    Add links: etsy, tiktok, youtube

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import ImgHeaderEtsy from '../images/header_etsy.png';
 import ImgHeaderInstagram from '../images/header_instagram.png';
 import ImgHeaderMastodon from '../images/header_mastodon.svg';
+import ImgHeaderTiktok from '../images/header_tiktok.png';
 import ImgHeaderTumblr from '../images/header_tumblr.png';
+import ImgHeaderYoutube from '../images/header_youtube.png';
 import { SocialBarIcon } from './SocialBarIcon';
 
 export const SocialBar = () => (
   <Container>
+    <SocialBarIcon
+      linkUrl={'https://www.etsy.com/shop/Skirtcraft'}
+      imageUrl={ImgHeaderEtsy}
+      altText={'Etsy'}
+    />
     <SocialBarIcon
       linkUrl={'https://www.instagram.com/skirtcraft'}
       imageUrl={ImgHeaderInstagram}
@@ -18,9 +26,19 @@ export const SocialBar = () => (
       altText={'Mastodon'}
     />
     <SocialBarIcon
+      linkUrl={'https://www.tiktok.com/@skirtcraft'}
+      imageUrl={ImgHeaderTiktok}
+      altText={'TikTok'}
+    />
+    <SocialBarIcon
       linkUrl={'https://skirtcraft.tumblr.com'}
       imageUrl={ImgHeaderTumblr}
       altText={'Tumblr'}
+    />
+    <SocialBarIcon
+      linkUrl={'https://www.youtube.com/@skirtcraft'}
+      imageUrl={ImgHeaderYoutube}
+      altText={'YouTube'}
     />
   </Container>
 );
