65e824d96501ba720aab483d7d21eedd7572bb77
Joe Quarion <2335027+jqrn@users.noreply.github.com>
2024-07-04 10:38:38 -0500

    Add facebook link to header and resize icon

--- src/components/SocialBar.tsx
+++ src/components/SocialBar.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import ImgHeaderEtsy from '../images/header_etsy.png';
+import ImgHeaderFacebook from '../images/header_facebook.png';
 import ImgHeaderInstagram from '../images/header_instagram.png';
-import ImgHeaderMastodon from '../images/header_mastodon.svg';
 import ImgHeaderThreads from '../images/header_threads.png';
 import ImgHeaderTiktok from '../images/header_tiktok.png';
 import ImgHeaderTumblr from '../images/header_tumblr.png';
@@ -21,14 +21,14 @@ export const SocialBar = () => (
       altText={'Threads'}
     />
     <SocialBarIcon
-      linkUrl={'https://mastodon.cloud/@skirtcraft'}
-      imageUrl={ImgHeaderMastodon}
-      altText={'Mastodon'}
-    />
-    <SocialBarIcon
       linkUrl={'https://www.tiktok.com/@skirtcraft'}
       imageUrl={ImgHeaderTiktok}
       altText={'TikTok'}
+    />
+    <SocialBarIcon
+      linkUrl={'https://facebook.com/skirtcraft'}
+      imageUrl={ImgHeaderFacebook}
+      altText={'Facebook'}
     />
     <SocialBarIcon
       linkUrl={'https://skirtcraft.tumblr.com'}
